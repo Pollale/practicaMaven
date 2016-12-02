7f13ea5dabf6a534b7dd26929049c2a093610a4e
Alejandro Tortolero <tortolero95@gmail.com>
2016-12-02 09:29:28 +0100

    Creación y avance de la práctica Maven hasta antes del despliegue

---
+++ src/main/java/es/egc/app/App.java
@@ -0,0 +1,23 @@
+package es.egc.app;
+import org.apache.log4j.*; 
+
+/**
+ * Hello world!
+ *
+ */
+ 
+ 
+public class App 
+{
+	static Logger log=Logger.getLogger(App.class); 
+	
+	static int getVal(){
+	return 1;
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		log.info("Returning 1");
+	
+    }
+}
